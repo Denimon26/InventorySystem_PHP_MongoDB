a7f04b1488afb216839fe14066771184aa869e1b
PZ991 <79073200+PZ991@users.noreply.github.com>
2024-11-01 15:56:51 +0800

    edit noerr

--- edit_group.php
+++ edit_group.php
@@ -1,71 +1,102 @@
 <?php
-  $page_title = 'Edit Group';
-  require_once('includes/load.php');
-  // Checkin What level user has permission to view this page
-   page_require_level(1);
-?>
-<?php
-  $e_group = find_by_id('user_groups',(int)$_GET['id']);
-  if(!$e_group){
-    $session->msg("d","Missing Group id.");
-    redirect('group.php');
+$page_title = 'Edit Group';
+require_once('includes/load.php');
+// Checkin What level user has permission to view this page
+page_require_level(1);
+
+require 'vendor/autoload.php';
+
+
+use MongoDB\Client;
+function page_require_level($required_level)
+{
+  $uri = 'mongodb+srv://boladodenzel:denzelbolado@cluster0.9ahxb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+  $client = new Client($uri);
+  $database = $client->selectDatabase('inventory_system');
+  $admins = $database->selectCollection('admin');
+  $admin = $admins->findOne(['_id' => $_SESSION['user_id']]);
+
+  if (!isset($admin)) {
+
+    redirect('login.php', false);
   }
+  if ($admin['user_level'] <= (int) $required_level) {
+    return true;
+  } else {
+    // If the user does not have permission, redirect to the home page
+    redirect('home.php', false);
+  }
+}
 ?>
 <?php
-  if(isset($_POST['update'])){
+$uri = 'mongodb+srv://boladodenzel:denzelbolado@cluster0.9ahxb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+$client = new Client($uri);
+$database = $client->selectDatabase('inventory_system');
+$groups = $database->selectCollection('group');
+$e_group = $groups->find(['_id'=>$_GET['id']]);
+if (!$e_group) {
+  $session->msg("d", "Missing Group id.");
+  redirect('group.php');
+}
+?>
+<?php
+if (isset($_POST['update'])) {
 
-   $req_fields = array('group-name','group-level');
-   validate_fields($req_fields);
-   if(empty($errors)){
-           $name = remove_junk($db->escape($_POST['group-name']));
-          $level = remove_junk($db->escape($_POST['group-level']));
-         $status = remove_junk($db->escape($_POST['status']));
+  $req_fields = array('group-name', 'group-level');
+  validate_fields($req_fields);
+  if (empty($errors)) {
+    $name = remove_junk($db->escape($_POST['group-name']));
+    $level = remove_junk($db->escape($_POST['group-level']));
+    $status = remove_junk($db->escape($_POST['status']));
 
-        $query  = "UPDATE user_groups SET ";
-        $query .= "group_name='{$name}',group_level='{$level}',group_status='{$status}'";
-        $query .= "WHERE ID='{$db->escape($e_group['id'])}'";
-        $result = $db->query($query);
-         if($result && $db->affected_rows() === 1){
-          //sucess
-          $session->msg('s',"Group has been updated! ");
-          redirect('edit_group.php?id='.(int)$e_group['id'], false);
-        } else {
-          //failed
-          $session->msg('d',' Sorry failed to updated Group!');
-          redirect('edit_group.php?id='.(int)$e_group['id'], false);
-        }
-   } else {
-     $session->msg("d", $errors);
-    redirect('edit_group.php?id='.(int)$e_group['id'], false);
-   }
- }
+    $query = "UPDATE user_groups SET ";
+    $query .= "group_name='{$name}',group_level='{$level}',group_status='{$status}'";
+    $query .= "WHERE ID='{$db->escape($e_group['id'])}'";
+    $result = $db->query($query);
+    if ($result && $db->affected_rows() === 1) {
+      //sucess
+      $session->msg('s', "Group has been updated! ");
+      redirect('edit_group.php?id=' . (int) $e_group['id'], false);
+    } else {
+      //failed
+      $session->msg('d', ' Sorry failed to updated Group!');
+      redirect('edit_group.php?id=' . (int) $e_group['id'], false);
+    }
+  } else {
+    $session->msg("d", $errors);
+    redirect('edit_group.php?id=' . (int) $e_group['id'], false);
+  }
+}
 ?>
 <?php //include_once('layouts/header.php'); ?>
 <div class="login-page">
-    <div class="text-center">
-       <h3>Edit Group</h3>
-     </div>
-     <?php echo display_msg($msg); ?>
-      <form method="post" action="edit_group.php?id=<?php echo (int)$e_group['id'];?>" class="clearfix">
-        <div class="form-group">
-              <label for="name" class="control-label">Group Name</label>
-              <input type="name" class="form-control" name="group-name" value="<?php echo remove_junk(ucwords($e_group['group_name'])); ?>">
-        </div>
-        <div class="form-group">
-              <label for="level" class="control-label">Group Level</label>
-              <input type="number" class="form-control" name="group-level" value="<?php echo (int)$e_group['group_level']; ?>">
-        </div>
-        <div class="form-group">
-          <label for="status">Status</label>
-              <select class="form-control" name="status">
-                <option <?php if($e_group['group_status'] === '1') echo 'selected="selected"';?> value="1"> Active </option>
-                <option <?php if($e_group['group_status'] === '0') echo 'selected="selected"';?> value="0">Deactive</option>
-              </select>
-        </div>
-        <div class="form-group clearfix">
-                <button type="submit" name="update" class="btn btn-info">Update</button>
-        </div>
-    </form>
+  <div class="text-center">
+    <h3>Edit Group</h3>
+  </div>
+  <?php echo display_msg($msg); ?>
+  <form method="post" action="edit_group.php?id=<?php echo (int) $e_group['id']; ?>" class="clearfix">
+    <div class="form-group">
+      <label for="name" class="control-label">Group Name</label>
+      <input type="name" class="form-control" name="group-name"
+        value="<?php echo remove_junk(ucwords($e_group['group_name'])); ?>">
+    </div>
+    <div class="form-group">
+      <label for="level" class="control-label">Group Level</label>
+      <input type="number" class="form-control" name="group-level" value="<?php echo (int) $e_group['group_level']; ?>">
+    </div>
+    <div class="form-group">
+      <label for="status">Status</label>
+      <select class="form-control" name="status">
+        <option <?php if ($e_group['group_status'] === '1')
+          echo 'selected="selected"'; ?> value="1"> Active </option>
+        <option <?php if ($e_group['group_status'] === '0')
+          echo 'selected="selected"'; ?> value="0">Deactive</option>
+      </select>
+    </div>
+    <div class="form-group clearfix">
+      <button type="submit" name="update" class="btn btn-info">Update</button>
+    </div>
+  </form>
 </div>
 
 <?php //include_once('layouts/footer.php'); ?>
