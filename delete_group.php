9ccc1232e9839544b95dcd0b279b19965628c017
PZ991 <79073200+PZ991@users.noreply.github.com>
2024-11-02 18:06:32 +0800

    edit group done

--- delete_group.php
+++ delete_group.php
@@ -6,6 +6,25 @@
 use MongoDB\Client;
   // Checkin What level user has permission to view this page
    page_require_level(1);
+
+   function page_require_level($required_level) {
+    $uri = 'mongodb+srv://boladodenzel:denzelbolado@cluster0.9ahxb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+    $client = new Client($uri);
+    $database = $client->selectDatabase('inventory_system');
+    $admins = $database->selectCollection('admin');
+    $admin=  $admins->findOne(['_id' => $_SESSION['user_id']]);
+  
+    if (!isset($admin)) {
+  
+        redirect('index.php', false);
+    }
+    if ($admin['user_level'] <= (int)$required_level) {
+        return true;
+    } else {
+        // If the user does not have permission, redirect to the home page
+        redirect('home.php', false);
+    }
+  }
 ?>
 <?php
 
--- edit_group.php
+++ edit_group.php
@@ -1,38 +1,78 @@
 <?php
-  $page_title = 'Edit Group';
-  require_once('includes/load.php');
-  // Checkin What level user has permission to view this page
-   //page_require_level(1);
-?>
-<?php
-  $e_group = find_by_id('user_groups',(int)$_GET['id']);
-  if(!$e_group){
-    $session->msg("d","Missing Group id.");
-    redirect('group.php');
+$page_title = 'Edit Group';
+require_once('includes/load.php');
+require 'vendor/autoload.php';
+use MongoDB\Client;
+
+// Checkin What level user has permission to view this page
+page_require_level(1);
+
+function page_require_level($required_level)
+{
+  $uri = 'mongodb+srv://boladodenzel:denzelbolado@cluster0.9ahxb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+  $client = new Client($uri);
+  $database = $client->selectDatabase('inventory_system');
+  $admins = $database->selectCollection('admin');
+  $admin = $admins->findOne(['_id' => $_SESSION['user_id']]);
+
+  if (!isset($admin)) {
+
+    redirect('index.php', false);
   }
-?>
-<?php
+  if ($admin['user_level'] <= (int) $required_level) {
+    return true;
+  } else {
+    // If the user does not have permission, redirect to the home page
+    redirect('home.php', false);
+  }
+}
+
+
+$uri = 'mongodb+srv://boladodenzel:denzelbolado@cluster0.9ahxb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+$client = new Client($uri);
+$database = $client->selectDatabase('inventory_system');
+$groups = $database->selectCollection('group');
+$group_id = $_GET['id'];
+$e_group = $groups->findOne(['group_name' => $group_id]);
+
+if (!$e_group) {
+  redirect('group.php');
+}
+
 if (isset($_POST['update'])) {
 
   $req_fields = array('group-name', 'group-level');
   validate_fields($req_fields);
   if (empty($errors)) {
-    $name = remove_junk($db->escape($_POST['group-name']));
-    $level = remove_junk($db->escape($_POST['group-level']));
-    $status = remove_junk($db->escape($_POST['status']));
+    $name = ($_GET['id']);
+    $level = remove_junk($_POST['group-level']);
+    $status = remove_junk($_POST['status']);
 
-    $query = "UPDATE user_groups SET ";
-    $query .= "group_name='{$name}',group_level='{$level}',group_status='{$status}'";
-    $query .= "WHERE ID='{$db->escape($e_group['id'])}'";
-    $result = $db->query($query);
-    if ($result && $db->affected_rows() === 1) {
+    $data = [
+      'group_name' => $name,
+      'group_level' => $level,
+      'group_status' => $status
+
+    ];
+    echo "<script>console.log('".$name."');</script>";
+    $result = $groups->updateOne(
+      ['group_name' => $name],
+      ['$set' => $data]
+    );
+    echo "<script>console.log('".json_encode($result)."');</script>";
+
+    if ($result->getModifiedCount() > 0) {
       //sucess
       $session->msg('s', "Group has been updated! ");
+      echo "<script>console.log('done');</script>";
       redirect('edit_group.php?id=' . (int) $e_group['id'], false);
     } else {
       //failed
       $session->msg('d', ' Sorry failed to updated Group!');
+      echo "<script>console.log('f');</script>";
+
       redirect('edit_group.php?id=' . (int) $e_group['id'], false);
+      
     }
   } else {
     $session->msg("d", $errors);
@@ -46,7 +86,7 @@ if (isset($_POST['update'])) {
     <h3>Edit Group</h3>
   </div>
   <?php echo display_msg($msg); ?>
-  <form method="post" action="edit_group.php?id=<?php echo (int) $e_group['id']; ?>" class="clearfix">
+  <form method="post" action="edit_group.php?id=<?php echo $_GET['id'] ?>">
     <div class="form-group">
       <label for="name" class="control-label">Group Name</label>
       <input type="name" class="form-control" name="group-name"
